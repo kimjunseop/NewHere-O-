0417713048bb2edf5b23bb9bfdac645476819dad
kimjunseop <newsic13@gmail.com>
2020-07-22 14:47:12 +0900

    Initial Commit

---
+++ NewHere,O!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewHere,O!
+//
+//  Created by GW_15 on 22/07/2020.
+//  Copyright © 2020 GW_15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
